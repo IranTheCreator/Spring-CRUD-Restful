fb3f6d9fbc76d9b3a289dbf755f001b57cb1fa56
Iranildo Alves <117860354+IranTheCreator@users.noreply.github.com>
2024-01-31 17:32:12 -0300

    Initial commit

---
+++ src/main/java/com/simntek/SpringCRUDRestful/SpringCrudRestfulApplication.java
@@ -0,0 +1,13 @@
+package com.simntek.SpringCRUDRestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrudRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCrudRestfulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simntek/SpringCRUDRestful/SpringCrudRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simntek.SpringCRUDRestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrudRestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
