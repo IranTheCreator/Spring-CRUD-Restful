9d270e3d050f0aed882bce5f3638f1c615a690a8
Iranildo Alves <117860354+IranTheCreator@users.noreply.github.com>
2024-01-31 19:25:13 -0300

    Second Commit

---
+++ src/main/java/com/simntek/SpringCRUDRestful/repositories/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.simntek.SpringCRUDRestful.repositories;
+
+import com.simntek.SpringCRUDRestful.models.ProductModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
+
+}
